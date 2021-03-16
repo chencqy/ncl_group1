43a730cbf341a66bb8119ee63755b825ebadb50b
Tom <tomrob270@gmail.com>
2021-03-16 13:10:58 +0000

    graph working

--- frontend2/src/services/user.service.js
+++ frontend2/src/services/user.service.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import authHeader from './auth-header'
 
 // edit this
+// const API_URL = 'http://localhost:8080/api/test/'
 const API_URL = 'http://18.132.43.65:8090/'
-// http://18.132.43.65:8090/
-
+const UNI_URL = 'https://api.usb.urbanobservatory.ac.uk/api/v2'
 // edit these methods
 class UserService {
   // Base level request - for home page
   getPublicContent () {
-    // get_default_rooms?
+    // public access
     return axios.get(API_URL + 'get_default_rooms')
   }
 
@@ -21,8 +21,15 @@ class UserService {
     return axios.get(API_URL + 'mod', { headers: authHeader() })
   }
 
+  // Graph page?
   getAdminBoard () {
     return axios.get(API_URL + 'admin', { headers: authHeader() })
+  }
+
+  // get roominfo method?
+  getRoomMetric (room, metric) {
+    var endpoint = '/sensors/timeseries/' + room + '/' + metric + '/raw/historic?startTime=2019-05-27T00:00:00Z&endTime=2019-05-29T23:59:59'
+    return axios.get(UNI_URL + endpoint)
   }
 }
 
