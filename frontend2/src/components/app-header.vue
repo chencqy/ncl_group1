854774427e121f0bb92db4fa29ecd041bcacd450
Tom <tomrob270@gmail.com>
2021-03-17 21:57:10 +0000

    add logout button

--- frontend2/src/components/app-header.vue
+++ frontend2/src/components/app-header.vue
@@ -4,11 +4,20 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/room">Room</router-link> |
       <router-link to="/dashboard">Dashboard</router-link> |
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login">Login</router-link> | | |
+      <button v-on:click="logOut()"> Logout</button>
     </div>
   </nav>
 </template>
 <script>
+export default {
+  methods: {
+    logOut () {
+      this.$store.dispatch('auth/logout')
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
 <style>
 </style>
